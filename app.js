243a24d716ea9763d92298d3b49a8efdb9098469
colin <clawrence005@yahoo.com>
2020-03-06 03:18:11 -0600

    added .env and mongo connection

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const product = require('./routes/product.route');
+require('dotenv').config();
 const app = express();
 
 app.use('/products', product);
@@ -9,3 +10,13 @@ app.use('/products', product);
 let PORT = 1234;
 
 app.listen(PORT, () => { console.log('\n >>>> Server is up and running on port', PORT, ' <<<<<') });
+
+// `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@claw85-l78sx.mongodb.net/test?retryWrites=true&w=majority`
+
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@claw85-l78sx.mongodb.net/test?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true
+  }
+);
+
