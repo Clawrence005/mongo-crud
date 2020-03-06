e9f47f517b150f079e0cb1f3fa4773d72f080fb7
colin <clawrence005@yahoo.com>
2020-03-06 15:40:50 -0600

    updating gitignore

--- app.js
+++ app.js
@@ -8,18 +8,22 @@ const app = express();
 
 app.use('/products', product);
 
-let PORT = 1234;
+let PORT = 1234 || process.env.PORT;
 
-app.listen(PORT, () => { console.log('\n >>>> Server is up and running on port', PORT, ' <<<<<') });
+
 
 // `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@claw85-l78sx.mongodb.net/test?retryWrites=true&w=majority`
 
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@claw85-l78sx.mongodb.net/test?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@claw85-l78sx.mongodb.net/test?retryWrites=true&w=majority`; console.log("\n >>>> DB Connected Successfully  <<<<<");
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 client.connect(err => {
   const collection = client.db("test").collection("devices");
+
   // perform actions on the collection object
   client.close();
 });
+
+app.listen(PORT, () => { console.log('\n >>>> Server is up and running on port', PORT, ' <<<<<') });
