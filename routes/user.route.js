ff90d8fc3674c6c4bbd79ffccdd7b650ce132018
colin <clawrence005@yahoo.com>
2020-03-10 02:11:08 -0500

    added user route

---
+++ routes/user.route.js
@@ -0,0 +1,8 @@
+express = require('express');
+const router = express.Router();
+
+const user_controller = require('../controllers/user.controller');
+//test
+router.get('test', user_controller.test);
+
+module.exports = router;
